857f3929927e7df657922864ac27ca6c5885d501
iaadi4 <adityasingh40675@gmail.com>
2025-09-21 06:56:31 +0530

    feat(http): add auth routes, fix(db): prisma client import error

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import authRoute from "./routes/auth-route";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -6,6 +7,10 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.json({ message: "Hello, World!" });
 })
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/auth', authRoute);
 
 export async function startServer() {
     app.listen(PORT);
